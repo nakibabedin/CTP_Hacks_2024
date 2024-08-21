aec5044e2e52852f8d2c8e7260f96fb2bee802f0
nakibabedin <nabedin30@stuy.edu>
2024-08-21 10:15:01 -0400

    use json for API commmunication

--- backend/app.py
+++ backend/app.py
@@ -1,12 +1,13 @@
 from flask import Flask, request
 from flask_cors import CORS
+import json
 
 app = Flask(__name__)
 CORS(app) 
 
 @app.route('/', methods=['GET'])
 def get_data():
-    return "Hello World"
+    return json.loads('{"message":"Hello World"}')
 
 if __name__ == '__main__':
     app.run(debug=True)
