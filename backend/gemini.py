9458ea83117806783cb6907a8cedba0de5d66893
nakibabedin <nabedin30@stuy.edu>
2024-08-21 12:10:08 -0400

    setup barebone gemini tool

--- backend/gemini.py
+++ backend/gemini.py
@@ -1,1 +1,14 @@
+import google.generativeai as genai
+
+
 # Read in the API Key from the local dot_env file
+with open('keys/gemini_api_key.txt', 'r') as file:
+    api_key = file.read()
+
+# configure with the Gemeni genai object
+genai.configure(api_key=api_key)
+model = genai.GenerativeModel('gemini-1.5-flash')
+
+response = model.generate_content("Say Hi to me in 5 words")
+print(response.text)
+
