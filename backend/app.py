5ca64078fec2fdd24b99d73613e604f4dff4dd3a
Tedd Lee <tedd0328@gmail.com>
2024-08-22 15:42:13 -0400

    Backend fixes to Gemini Calls

--- backend/app.py
+++ backend/app.py
@@ -5,21 +5,22 @@ import firebase
 import keywords_parser
 
 app = Flask(__name__)
-CORS(app) 
+CORS(app)
 
-@app.route('/', methods=['GET'])
-def get_data():
-    return json.loads('{"message":"Hello World"}')
+# @app.route('/', methods=['GET'])
+# def get_data():
+#     return json.loads('{"message":"Hello World"}')
 
 @app.route('/createResource', methods=['POST'])
 def create_resource():
     frontend_request = request.get_json()
+#     print(frontend_request)
     campus_code = frontend_request['campus']
     resource_name = frontend_request['name']
     resource_data = keywords_parser.process_description(frontend_request['data'])
 
     firebase.add_resource(campus_code, resource_name, resource_data)
-    return "hello world"
+    return json.loads('{"message":"success"}')
 
 if __name__ == '__main__':
     app.run(debug=True)
