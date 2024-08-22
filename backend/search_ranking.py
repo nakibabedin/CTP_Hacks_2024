b75a9a369e35447241c5247b6520d7a9126a5424
Alif Rahman <alifrahman2023@gmail.com>
2024-08-22 14:56:14 -0400

    finished ranking system

--- backend/search_ranking.py
+++ backend/search_ranking.py
@@ -1,4 +1,5 @@
 import firebase
+import heapq
 
 def getJaccardSimilarity(target_keywords, resource_keywords):
     target_set = set(target_keywords)
@@ -15,10 +16,24 @@ def getJaccardSimilarity(target_keywords, resource_keywords):
 def rank_resources(campus, target_keywords, amount_desired):
     resources_for_campus = firebase.get_data_for_campus(campus) 
     resources_cunywide = firebase.get_data_for_campus('CUNY')
-    
+    total_resources = resources_for_campus.append(resources_cunywide)
+
     rankings = []
 
-    for resource in resources_for_campus:
+    for resource in total_resources:
+        # find Jaccard similarity
         resource_keywords = resource['keywords']
         current_jaccard_similarity = getJaccardSimilarity(target_keywords, resource_keywords)
-        
+        # only find top-amount_desired resources by using heap
+        if len(rankings) == amount_desired:
+            heapq.heappop(rankings)
+        heapq.heappush(rankings, (-current_jaccard_similarity, resource))
+    
+    # since rankings is sorted as the minimum similarity having the most priority, 
+    # append to result by traversing from the end to beginning of rankings
+    result = []
+    for i in range( len(rankings)-1, -1, -1):
+        result.append(rankings[i][1])
+    
+    return result
+
