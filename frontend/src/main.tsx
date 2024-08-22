241808c65e6af9d5e1e86724b46da416bf6b5e4c
Tedd Lee <tedd0328@gmail.com>
2024-08-22 14:25:00 -0400

    Add Router + Create Page

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,27 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import {
+    createBrowserRouter,
+    RouterProvider,
+} from "react-router-dom";
+
 import App from './App.tsx'
+import Create from './Create.tsx'
 import './index.css'
+
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <App />,
+    },
+    {
+        path: "/create",
+        element: <Create />,
+    }
+]);
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+      <RouterProvider router={router} />
   </StrictMode>,
 )
